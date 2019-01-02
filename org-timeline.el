9acab5adcf6bfb7baf182224fb55a7ffd07de944
Matus Goljer <matus.goljer@gmail.com>
2019-01-02 21:43:59 +0100

    Fix description

--- org-timeline.el
+++ org-timeline.el
@@ -35,7 +35,7 @@
 ;; display with `org-timeline-block' face.  Clocked entires are
 ;; displayed in `org-timeline-clocked' face.  The background of
 ;; timeslots which are in the past is highlighted with
-;; `org-timeline-elapsed` face.
+;; `org-timeline-elapsed' face.
 
 ;; You can use custom color for a task by adding the property
 ;; `TIMELINE_FACE' with either a string which is a color name or a
